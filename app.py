34ca1d0781a0695cc941f86429ce50c41b2789f3
pechiyadav <77777433+pechiyadav@users.noreply.github.com>
2025-03-17 10:01:25 +0530

    Updated the app.py file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 
 def greet():
   current_time = datetime.datetime.now()
-  print(f"Hello from modified Jenkins at {current_time}!")
+  print(f"Hello from Jenkins using polling at {current_time}!")
 
 if __name__ == "__main__":
   greet()
